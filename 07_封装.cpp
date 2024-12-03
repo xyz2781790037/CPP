aa62ee5388f7ceb91341587f94e77fb866270fe7
xyz2781790037 <2781790037@qq.com>
2024-12-03 15:52:53 +0800

    newfile

--- 07_封装.cpp
+++ 07_封装.cpp
@@ -26,21 +26,31 @@ public:
 };
 class Person
 {
-public://类内可以访问，类外可以
+public: // 类内可以访问，类外可以
     string m_name;
+
 protected: // 类内可以访问，类外不可以]
     string m_car;
+
 private: // 类内可以访问，类外不可以
     int m_password;
-public:
-void func()
-{
-    m_name = "zyx";
-    m_car = "baoam";
-    m_password = 12345;
-}
-};
 
+public:
+    void func()
+    {
+        m_name = "zyx";
+        m_car = "baoam";
+        m_password = 12345;
+    }
+};
+class c1
+{
+    int a;//默认为private
+};
+struct c3
+{
+    int a;//默认为public
+};
 int main()
 {
     // Circle cl;
@@ -53,6 +63,10 @@ int main()
     // cout << st.id << endl;
     Person p1;
     p1.m_name = "sf";
-    //p1.m_car = "d";不可访问
+    // p1.m_car = "d";不可访问
+    c1 c2;
+    c3 c4;
+    //c2.a = 3;->error
+    c4.a = 4;
     return 0;
 }

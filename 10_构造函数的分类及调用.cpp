145a0c6e21881f43591bc481b322a554c60f0585
xyz2781790037 <2781790037@qq.com>
2024-12-04 20:23:20 +0800

    newfile

--- 10_构造函数的分类及调用.cpp
+++ 10_构造函数的分类及调用.cpp
@@ -36,7 +36,21 @@ public:
 void test1()
 {
     Person p1;
+    // 注意1：调用无参构造函数不能加括号，如果加了编译器认为这是一个函数声明
+    // Person p1();
+    // 括号法
     Person p2(10);
+    Person p3(p2);
+    // cout << p3.age << endl;
+    // 显式法
+    Person p4 = Person(10);
+    Person p5 = Person(p2);
+    // Person(10) 单独写就是匿名对象  当前行结束之后，马上析构
+    // 不能利用 拷贝构造函数 初始化匿名对象 编译器认为是对象声明
+    // Person p5(p4);
+    // 隐式转换法
+    Person p4 = 10; // Person p4 = Person(10);
+    Person p5 = p4; // Person p5 = Person(p4);
 }
 int main()
 {

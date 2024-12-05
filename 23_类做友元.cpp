69937894ff447b89d82eba4013246f83925da719
xyz2781790037 <2781790037@qq.com>
2024-12-05 22:25:35 +0800

    newfile

---
+++ 23_类做友元.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+class Building;
+class goodGay
+{
+public:
+    goodGay();
+    void visit();
+
+    // private:
+    Building *building;
+};
+class Building
+{
+    friend void goodGay::visit();
+
+public:
+    Building();
+
+public:
+    string m_SittingRoom; // 客厅
+private:
+    string m_BedRoom; // 卧室
+};
+Building::Building()
+{
+    this->m_SittingRoom = "客厅";
+    this->m_BedRoom = "卧室";
+}
+goodGay::goodGay()
+{
+    building = new Building;
+}
+void goodGay::visit()
+{
+    cout << "好基友正在访问" << building->m_SittingRoom << endl;
+    cout << "好基友正在访问" << building->m_BedRoom << endl;
+}
+void test01()
+{
+    goodGay gg;
+    gg.visit();
+}
+int main()
+{
+    test01();
+    return 0;
+}

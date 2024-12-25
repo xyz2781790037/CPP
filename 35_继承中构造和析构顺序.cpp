8499e6d40b028b6c45c3ba093f02c4e35152b7f9
xyz2781790037 <2781790037@qq.com>
2024-12-25 17:12:13 +0800

    newfile

--- 35_继承中构造和析构顺序.cpp
+++ 35_继承中构造和析构顺序.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 using namespace std;
-
+class Base
+{
+public:
+    Base()
+    {
+        cout << "Base构造函数!" << endl;
+    }
+    ~Base()
+    {
+        cout << "Base析构函数!" << endl;
+    }
+};
+class Son : public Base
+{
+public:
+    Son()
+    {
+        cout << "Son构造函数!" << endl;
+    }
+    ~Son()
+    {
+        cout << "Son析构函数!" << endl;
+    }
+};
+void test01()
+{
+    // 继承中 先调用父类构造函数，再调用子类构造函数，析构顺序与构造相反
+    Son s;
+}
 int main()
 {
+    test01();
     return 0;
 }
+// 总结：继承中先调用父类构造函数，再调用子类构造函数，构造顺序与构造正常

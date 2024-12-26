9c0187feca03b7c9f75f44bdc1010c653103d903
xyz2781790037 <2781790037@qq.com>
2024-12-26 14:40:46 +0800

    newfile

--- 45_文本文件.cpp
+++ 45_文本文件.cpp
@@ -20,10 +20,10 @@ using namespace std;
 // 关闭文件
 // ofs.close()；
 // ios::in	    为读文件而打开文件
-// ios::out	为写文件而打开文件
-// ios::ate	初始位置：文件尾
-// ios::app	追加方式写文件
-// ios::trunc	如果文件存在先删除，再创建
+// ios::out	    为写文件而打开文件
+// ios::ate	    初始位置：文件尾
+// ios::app	    追加方式写文件
+// ios::trunc 	如果文件存在先删除，再创建
 // ios::binary	二进制方式
 // 文件打开方式可以配合使用，利用|操作符
 void test1()
--- 46_读文件.cpp
+++ 46_读文件.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iostream>
 using namespace std;
 // 读文件与写文件步骤相似，但是读取方式相对于比较多
 // 读文件步骤如下：
@@ -12,8 +13,52 @@ using namespace std;
 // 四种方式读取
 // 关闭文件
 // ifs.close();
+void test01()
+{
+    ifstream ifs;
+    ifs.open("45_test.txt", ios::in);
+
+    if (!ifs.is_open())
+    {
+        cout << "文件打开失败" << endl;
+        return;
+    }
+
+    // 第一种方式
+    // char buf[1024] = { 0 };
+    // while (ifs >> buf)
+    //{
+    //	cout << buf << endl;
+    // }
+
+    // 第二种
+    // char buf[1024] = { 0 };
+    // while (ifs.getline(buf,sizeof(buf)))
+    //{
+    //	cout << buf << endl;
+    // }
+
+    // 第三种
+    // string buf;
+    // while (getline(ifs, buf))
+    //{
+    //	cout << buf << endl;
+    // }
+
+    char c;
+    while ((c = ifs.get()) != EOF)
+    {
+        cout << c;
+    }
+
+    ifs.close();
+}
+
 int main()
 {
-    
+    test01();
     return 0;
 }
+// 读文件可以利用 ifstream ，或者fstream类
+// 利用is_open函数可以判断文件是否打开成功
+// close 关闭文件

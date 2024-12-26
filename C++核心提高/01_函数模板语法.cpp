072420dc2fc6a897f6b78c8ca8307f3ca1fc0dd3
xyz2781790037 <2781790037@qq.com>
2024-12-26 18:14:19 +0800

    newfile

---
+++ C++核心提高/01_函数模板语法.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// 函数模板作用：
+// 建立一个通用函数，其函数返回值类型和形参类型可以不具体制定，用一个虚拟的类型来代表。
+// template<typename T> 函数声明或定义
+// template --- 声明创建模板
+// typename --- 表面其后面的符号是一种数据类型，可以用class代替
+// T --- 通用的数据类型，名称可以替换，通常为大写字母
+int main()
+{
+    return 0;
+}

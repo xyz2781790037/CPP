585ada395db4c9265188160bf1a38f44654c8343
xyz2781790037 <2781790037@qq.com>
2025-01-15 15:55:00 +0800

    newfile

---
+++ C++核心提高/18_Vector存放自定义数据类型.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Person
+{
+public:
+    Person(string name,int age)
+    {
+        this->m_Age = age;
+        this->m_Name = name;
+    }
+    string m_Name;
+    int m_Age;
+};
+void test1()
+{
+    vector<Person> v;
+    // 创建数据
+    Person p1("aaa", 10);
+    Person p2("bbb", 20);
+    Person p3("ccc", 30);
+    Person p4("ddd", 40);
+    Person p5("eee", 50);
+    v.push_back(p1);
+    v.push_back(p2);
+    v.push_back(p3);
+    v.push_back(p4);
+    v.push_back(p5);
+    for (vector<Person>::iterator it = v.begin(); it != v.end();it++)
+    {
+        cout << "Name:" << it->m_Name << " Age:" << it->m_Age << endl;
+    }
+}
+void test02()
+{
+
+    vector<Person *> v;
+
+    // 创建数据
+    Person p1("aaa", 10);
+    Person p2("bbb", 20);
+    Person p3("ccc", 30);
+    Person p4("ddd", 40);
+    Person p5("eee", 50);
+
+    v.push_back(&p1);
+    v.push_back(&p2);
+    v.push_back(&p3);
+    v.push_back(&p4);
+    v.push_back(&p5);
+
+    for (vector<Person *>::iterator it = v.begin(); it != v.end();it++)
+    {
+        // cout << "Name:" << (*it)->m_Name << "Age:" << (*it)->m_Age << endl;//the first way
+        Person *p = (*it);
+        cout << "Name:" << p->m_Name << " Age:" << (*it)->m_Age << endl;
+    }
+}
+int main()
+{
+    return 0;
+}

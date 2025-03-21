a44dfd6cecd9cb6facf309aa5c6d3d33e0cc9e0e
xyz2781790037 <2781790037@qq.com>
2025-03-21 20:56:28 +0800

    newfile

--- C++核心提高/56_forward_list.cpp
+++ C++核心提高/56_forward_list.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <forward_list>
+#include <algorithm>
 // push_front(value) 只能在头部插入
 // 指定位置插入
 // insert_after(it, value);
@@ -10,6 +11,8 @@
 // 获取头部元素
 // front()；
 // before_begin()；
+// std::forward_list<int> fl2(fl1);
+
 using namespace std;
 int main(){
     forward_list<int> fl = {1, 2, 3, 4, 5};
@@ -17,8 +20,33 @@ int main(){
     fl.erase_after(it);
     it++;
     cout << *it << endl;
+    // std::forward_list<int> fl1 = {1, 2, 3, 4, 5};
+    // std::forward_list<int> fl2;
+
+    // fl2 = fl1; // 赋值操作
     for (int num : fl)
     {
         cout << num << " ";
     }
+    // std::forward_list<int> fl;
+
+    // // 用列表赋值
+    // fl.assign({1, 2, 3, 4, 5});
+
+    // std::cout << "fl: ";
+    // for (int num : fl)
+    // {
+    //     std::cout << num << " ";
+    // }
+    // std::cout << std::endl;
+
+    // // 用多个相同的值赋值
+    // fl.assign(3, 100); // 赋值 3 个 100
+
+    // std::cout << "fl after assign(3, 100): ";
+    // for (int num : fl)
+    // {
+    //     std::cout << num << " ";
+    // }
+    // return 0;
 }
